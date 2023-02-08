c87f3836c768f1f0b05f9c3e497187eca65eb248
Leonel Juarez <leonel.diego.juarez@gmail.com>
2023-02-07 22:12:16 -0300

    Avance PreEntregaTres

--- index.js
+++ index.js
@@ -1,115 +1,88 @@
-class Celulares{
 
-    constructor ( marca, precio ,stock){
-        
-        this.marca=marca;
-        this.precio=precio;
-        this.stock=stock;
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+function agregar_carrito(e){
+
+    console.log("Pclick" , e.target);
+
+    let datos = e.target.parentNode.parentNode;
+    
+    //let padre = hijo.parentNode;
+  // let abuelo = padre.parentNode;
+   // console.log(padre);
+
+    let nombre_producto = datos.querySelector("h5").textContent;
+    let precio_producto = datos.querySelector("p").textContent;
+
+    console.log(nombre_producto);
+    console.log(precio_producto);
+
+    let producto = {
+        nombre : nombre_producto,
+        precio : precio_producto,
+        cantidad : 1
+    };
+
+    mostrar_carrito(producto);
+}
+
+
+
+
+function mostrar_carrito(producto){
+
+    let fila = document.createElement("tr");
+
+    fila.innerHTML =    `<td> ${producto.nombre} </td>
+                        <td> ${producto.cantidad} </td>
+                        <td> ${producto.precio}</td>
+                        <td> <button class="btn btn-danger borrar">Borrar</button></td>
+                        `;
+
+    let tabla = document.getElementById("tbody");
+    tabla.append(fila);
+
+
+    // BOTON BORRAR
+    let btn_borrar = document.querySelectorAll(".borrar");
+    for (let boton of btn_borrar){
+        boton.addEventListener("click", borrar)
     }
+}
 
-    get_datos(){
-        console.log("<------------>")
-        console.log("Nombre:", this.marca);
-        console.log("Precio" ,this.precio);
-        console.log("Stock:" ,this.stock);
-        console.log("");
-    }
 
-    get_stock(){
-        if (this.stock <= 0){
-        return false;
+
+//BOTON BORRAR
+
+function borrar(e){
+    e.target.parentNode.parentNode.remove();
+}
+
+
+
+//BOTON COMPRA
+
+let boton_compra = document.querySelectorAll (".btn_compra");
+
+console.log(boton_compra);
+
+for (let boton of boton_compra){
+
+    boton.addEventListener("click" , agregar_carrito);
+}
+
+//BOTON CARRITO
+
+let btn_carrito = document.getElementById("mostrar_carrito");
+
+btn_carrito.addEventListener("click", function(){
+
+    let carrito = document.getElementById("carrito");
+
+    if( carrito.style.display != "none"){
+        carrito.style.display = "none";
     }else{
-        return true;
+        carrito.style.display = "block";
     }
-}
-  
-    update_stock(cantidad){
-        if(this.stock>=cantidad){
-            this.stock = this.stock - cantidad;
-            return true;
-        }else{
-            console.log("Stock Bajo");
-            return false;
-        }
-    }
-
-
-}
-
-//ALTA PRODUCTOS
-
-let lista_celulares = [] ;
-lista_celulares.push (new Celulares("Samsung A50",70000,20));
-lista_celulares.push (new Celulares("Samsung A75",80000,10));
-lista_celulares.push (new Celulares("Motorola E32",75000,12));
-lista_celulares.push (new Celulares("Motorola G100",95000,5));
-
-lista_celulares.push(new Celulares("Bateria A50",5000,10));
-lista_celulares.push(new Celulares("Bateria A75",6000,7));
-lista_celulares.push(new Celulares("Bateria E32",4000,6));
-lista_celulares.push(new Celulares("Bateria G100",5000,2));
-
-console.log (lista_celulares);
-
-//FIN ALTA
-
-//RENDER
-for (let celulares of lista_celulares){
-    celulares.get_datos();
-}
-
-
-//FIN RENDER
-
-//COMPRA
-
-function buscar (celulares){
-
-    return celulares.marca == compra;
-}
-
-
-let compra = prompt ("QUE DESEAS COMPRAR?");
-
-let resultado  = lista_celulares.find(buscar);
-
-console.log(resultado);
-
-
-
-
-
-
-if (resultado != undefined ) {
-
-    //COMPRA
-    
-    if (resultado.get_stock()){
-        let unidades = prompt("Cuantas deseas comprar?");
-    
-    
-        if(resultado.update_stock (unidades)){
-
-                console.log("Compra realizadad con exito");
-
-            }else{
-                   
-
-                console.log("No se puede realizar la compra");
-                
-        }
-
-    }
-
-}else{
-    console.log("PRODUCTO NO ENCONTRADO");
-}
-
-
-
-
-
-
-
+})
 
